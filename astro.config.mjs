42946ce5ddf23be0a67d9e771888e3d3ef92bfc9
sailor0913 <sailor0913@163.com>
2025-01-06 04:54:03 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import tailwind from '@astrojs/tailwind';
+
+import markdoc from '@astrojs/markdoc';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind(), markdoc(), mdx(), sitemap()]
+});
